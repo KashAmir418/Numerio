d96ef4cf67a684d04255e546d8842914f4a947d5
KashAmir418 <kashamir418@gmail.com>
2026-01-02 17:24:29 +0100

    Switch PostHog default host to EU region

--- src/components/providers/posthog-provider.tsx
+++ src/components/providers/posthog-provider.tsx
@@ -2,22 +2,49 @@
 
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
-import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect, Suspense } from "react";
 
 if (typeof window !== "undefined") {
     const key = process.env.NEXT_PUBLIC_POSTHOG_KEY;
-    const host = process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com";
+    const host = process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com";
 
     if (key) {
         posthog.init(key, {
             api_host: host,
-            person_profiles: "identified_only", // or 'always' if you want to track anonymous users as unique persons
-            capture_pageview: false, // We'll handle this manually for more accuracy
+            person_profiles: "identified_only",
+            capture_pageview: false, // Handled manually below
             capture_pageleave: true,
         });
     }
 }
 
+function PostHogPageview() {
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        if (pathname) {
+            let url = window.origin + pathname;
+            if (searchParams.toString()) {
+                url = url + `?${searchParams.toString()}`;
+            }
+            posthog.capture("$pageview", {
+                $current_url: url,
+            });
+        }
+    }, [pathname, searchParams]);
+
+    return null;
+}
+
 export function PostHogAuth({ children }: { children: React.ReactNode }) {
-    return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+    return (
+        <PostHogProvider client={posthog}>
+            <Suspense fallback={null}>
+                <PostHogPageview />
+            </Suspense>
+            {children}
+        </PostHogProvider>
+    );
 }
